cb334a4111b7864d018cb7a98b6e9fd1ba896222
GrishaSav <savchenko.grish@gmail.com>
2023-05-01 07:41:38 +0300

    2 js task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Vimeo from '@vimeo/player';
+import _throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+const player = new Vimeo('vimeo-player');
+
+player.on('loaded', () => {
+  const currentTime = localStorage.getItem(STORAGE_KEY);
+  player.setCurrentTime(currentTime);
+});
+
+player.on(
+  'timeupdate',
+  _throttle(({ seconds }) => {
+    localStorage.setItem(STORAGE_KEY, seconds);
+  }, 1000)
+);
