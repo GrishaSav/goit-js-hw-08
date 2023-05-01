e69e0eae2dbc1ad405459abd1b41be6e0fbf6d54
GrishaSav <savchenko.grish@gmail.com>
2023-05-01 07:42:25 +0300

    3 js task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import _throttle from 'lodash.throttle';
+const formEl = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
 
+let formValue = {};
+
+loadForm();
+
+formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', _throttle(onFormInput, 500));
+
+function onFormInput(evt) {
+  formValue = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+  formValue[evt.target.name] = evt.target.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formValue));
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  console.log(formValue);
+}
+
+function loadForm() {
+  let savedForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (!savedForm) {
+    return;
+  }
+
+  formEl.email.value = savedForm.email || '';
+  formEl.message.value = savedForm.message || '';
+}
